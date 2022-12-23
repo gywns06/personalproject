44bc39a21d9d72328516e286809f825f7184949e
gywns06 <119536143+gywns06@users.noreply.github.com>
2022-12-23 20:56:29 +0100

    updated foodquantityoningredientlist

--- ingredientlist.js
+++ ingredientlist.js
@@ -9,65 +9,33 @@ fetch("http://localhost:3000/Fooditem")
       //TO DO: create a 'tr element' and append it to the ingreidentlist, *egg and other items have different tr elements*
       // for loop goes through every element in a list, and the number in a for loop is defined here as i  // we defined i as 0, so i begins from 0. --> then, it deices if i is smaller than json.length or not --> if it is smaller than json.length, the jquery tag below starts running --> after that, it moves to the next step which is adding 1 to the variable 'i'
       // for (define start, define end, define interval i = i + 1)
-      if (json[i].name == "egg") {
-        $("#ingredienttable").append(
-          "<tr>" +
-            "<td>" +
-            json[i].name +
-            "</td>" +
-            "<td>" +
-            json[i].quantity +
-            "</td>" +
-            "<td>" +
-            json[i].expiredate +
-            "</td>" +
-            "<td>" +
-            json[i].description +
-            "</td>" +
-            "<td>" +
-            "<button type='button' onclick='editbutton(" +
-            json[i].id +
-            ")'>Click here!</button>" +
-            "</td>" +
-            "<td>" +
-            "<button type='button' onclick='deletebutton(" +
-            json[i].id +
-            ")'>Click here!</button>" +
-            "</td>" +
-            "</tr>"
-        );
-      } else if (
-        json[i].name == "cheese" ||
-        json[i].name == "pork" ||
-        json[i].name == "beef"
-      ) {
-        $("#ingredienttable").append(
-          "<tr>" +
-            "<td>" +
-            json[i].name +
-            "</td>" +
-            "<td>" +
-            json[i].quantity +
-            "</td>" +
-            "<td>" +
-            json[i].expiredate +
-            "</td>" +
-            "<td>" +
-            json[i].description +
-            "</td>" +
-            "<td>" +
-            "<button type='button' onclick='editbutton(" +
-            json[i].id +
-            ")'>Click here!</button>" +
-            "</td>" +
-            "<td>" +
-            "<button type='button' onclick='deletebutton(" +
-            json[i].id +
-            ")'>Click here!</button>" +
-            "</td>" +
-            "</tr>"
-        );
-      }
+
+      $("#ingredienttable").append(
+        "<tr>" +
+          "<td>" +
+          json[i].name +
+          "</td>" +
+          "<td>" +
+          json[i].quantity +
+          "</td>" +
+          "<td>" +
+          json[i].expiredate +
+          "</td>" +
+          "<td>" +
+          json[i].description +
+          "</td>" +
+          "<td>" +
+          "<button type='button' onclick='editbutton(" +
+          json[i].id +
+          ")'>Click here!</button>" +
+          "</td>" +
+          "<td>" +
+          "<button type='button' onclick='deletebutton(" +
+          json[i].id +
+          ")'>Click here!</button>" +
+          "</td>" +
+          "</tr>"
+      );
     }
   });
 
@@ -82,6 +50,34 @@ function deletebutton(id) {
   });
   location.reload();
 }
+
+function showquantity() {
+  $("#quantity").removeClass("donotshow");
+  $("#quantitylabel").removeClass("donotshow");
+  $("#quantityingrams").addClass("donotshow");
+  $("#quantityingramslabel").addClass("donotshow");
+}
+
+function showquantityingrams() {
+  $("#quantity").addClass("donotshow");
+  $("#quantitylabel").addClass("donotshow");
+  $("#quantityingrams").removeClass("donotshow");
+  $("#quantityingramslabel").removeClass("donotshow");
+}
+
+let quantitydictionary = {
+  egg: showquantity,
+  cheese: showquantityingrams,
+  beef: showquantityingrams,
+  pork: showquantityingrams,
+};
+
+let foodtypedictionary = {
+  egg: { quantity: "#quantity", type: "milkproduct" },
+  cheese: { quantity: "#quantityingrams", type: "milkproduct" },
+  beef: { quantity: "#quantityingrams", type: "meat" },
+  pork: { quantity: "#quantityingrams", type: "meat" },
+};
 
 function editbutton(id) {
   console.log(id);
@@ -100,40 +96,16 @@ function editbutton(id) {
       $("#expiredate").val(json.expiredate);
       $("#fooddescription").text(json.description);
 
-      if (json.name == "egg") {
-        $("#quantity").removeClass("donotshow");
-        $("#quantitylabel").removeClass("donotshow");
-        $("#quantityingrams").addClass("donotshow");
-        $("#quantityingramslabel").addClass("donotshow");
-      } else if (
-        json.name == "cheese" ||
-        json.name == "beef" ||
-        json.name == "pork"
-      ) {
-        $("#quantity").addClass("donotshow");
-        $("#quantitylabel").addClass("donotshow");
-        $("#quantityingrams").removeClass("donotshow");
-        $("#quantityingramslabel").removeClass("donotshow");
-      }
+      quantitydictionary[json.name]();
 
       $("#savebutton").click(function () {
         var food = $("#food").val();
-        var quantity;
-        if (food == "egg") {
-          quantity = $("#quantity").val();
-        } else if (food == "cheese" || food == "beef" || food == "pork") {
-          quantity = $("#quantityingrams").val();
-        }
+        var quantity = $(foodtypedictionary[food].quantity).val();
 
         var expiredate = $("#expiredate").val();
         var fooddescription = $("#fooddescription").val();
 
-        var type = json.type;
-        if (food == "egg" || food == "cheese") {
-          type = "milkproduct";
-        } else if (food == "beef" || food == "pork") {
-          type = "meat";
-        }
+        var type = foodtypedictionary[food].type;
 
         fetch("http://localhost:3000/Fooditem/" + id, {
           method: "PUT",
@@ -160,15 +132,5 @@ function editbutton(id) {
 
 $("#food").change(function () {
   fooditem = document.getElementById("food").value;
-  if (fooditem == "egg") {
-    $("#quantity").removeClass("donotshow");
-    $("#quantitylabel").removeClass("donotshow");
-    $("#quantityingrams").addClass("donotshow");
-    $("#quantityingramslabel").addClass("donotshow");
-  } else if (fooditem == "cheese" || fooditem == "beef" || fooditem == "pork") {
-    $("#quantity").addClass("donotshow");
-    $("#quantitylabel").addClass("donotshow");
-    $("#quantityingrams").removeClass("donotshow");
-    $("#quantityingramslabel").removeClass("donotshow");
-  }
+  quantitydictionary[fooditem]();
 });
